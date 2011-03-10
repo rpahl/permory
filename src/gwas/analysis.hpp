7a7bbb08f6f78aeacd086b2de548266b8b56a222
Volker Steiß <volker.steiss@mni.th-mittelhessen.de>
2011-03-10 12:16:19 +0100

    Created class Analyzer.

--- src/gwas/analysis.hpp
+++ src/gwas/analysis.hpp
@@ -26,6 +26,166 @@
 #include "statistical/pvalue.hpp"
 
 namespace Permory { namespace gwas {
+
+    template<uint K, uint L> class Analyzer {
+        public:
+            // Ctor
+            Analyzer(
+                    detail::Parameter* par, io::Myout& out,
+                    const std::vector<bool>& trait, Gwas* study,
+                    std::set<char> the_domain=std::set<char>())
+                : par_(par), out_(out), trait_(trait), study_(study),
+                  domain_(the_domain)
+                {}
+
+            // Modifiers
+            void analyze_dichotom();
+
+        private:
+            detail::Parameter* par_;
+            io::Myout& out_;
+            const std::vector<bool>& trait_;
+            Gwas* study_;
+            std::set<char> domain_;
+    };
+
+    // Analyzer implementation
+    // ========================================================================
+
+
+    //
+    //  Compute test statistics and perform permutation test
+    template<uint K, uint L> void Analyzer<K,L>::analyze_dichotom()
+    {
+        using namespace std;
+        using namespace boost;
+        using namespace io;
+        using namespace statistic;
+
+        // Prepare progress bar
+        size_t m = study_->m();
+        size_t perm_todo = par_->nperm_total;        //remaining permutations
+        scoped_ptr<progress_display> pprogress;
+        bool show_progress = not par_->quiet;
+        if (show_progress) {
+            double d = ceil(double(perm_todo)/double(par_->nperm_block));
+            pprogress.reset(new progress_display(m*size_t(d), std::cout, "","",""));
+        }
+
+        // Define locus filter (e.g. maf filter)
+        ptr_vector<Locus_filter> locus_filters;
+        locus_filters.push_back(new Maf_filter(par_->min_maf, par_->max_maf));
+        locus_filters.push_back(new Polymorph_filter());
+
+        deque<double> tmax;     //holds the maximum test statistic per permutation
+        statistic::Dichotom<K,L> stat(*par_, trait_); //computes all statistic stuff
+        permutation::Permutation pp(par_->seed);     //does the shuffling/permutation
+        Gwas::iterator itLocus = study_->begin();    //points to current locus
+
+        bool isFirstRound = true;
+        while (perm_todo > 0) {
+            // By default analysis is done in blocks of 10000 permutations. In
+            // each block each marker is analyzed one by one
+            size_t nperm = par_->nperm_block;
+            if (nperm > perm_todo) {
+                nperm = perm_todo;
+            }
+            stat.renew_permutations(&pp, nperm, par_->tail_size); //fresh random numbers
+            itLocus = study_->begin(); 
+
+            BOOST_FOREACH(string fn, par_->fn_marker_data) { 
+                Locus_data_reader<char> loc_reader(fn, par_->undef_allele_code);
+
+                while (loc_reader.hasData()) {
+                    std::vector<char> v;
+                    loc_reader.get_next(v);
+                    Locus_data<char> data(v, par_->undef_allele_code);
+                    if (data.size() == 0) {
+                        continue;
+                    }
+
+                    if (par_->marker_type == genotype) {
+                        if (data.size() == 2*trait_.size()) {
+                            data = data.condense_alleles_to_genotypes(2);
+                        }
+                        // Ensure that all possible genotypes appear in the domain
+                        data.add_to_domain(domain_);
+                    }
+
+                    if (data.size() != trait_.size()) {
+                        // Provide some more information in case of this error
+                        // as it may be hard to spot in large data sets
+                        std::string s = "At marker no ";
+                        s.append(boost::lexical_cast<string>(itLocus->id()));
+                        s.append(": length of phenotype data (");
+                        s.append(boost::lexical_cast<string>(trait_.size()));
+                        s.append(") does not match with length of marker data (");
+                        s.append(boost::lexical_cast<string>(data.size()));
+                        s.append(").\n");
+                        if (data.size() == 2*trait_.size()) {
+                            s.append("Maybe you forgot to set option '--allelic'?\n");
+                        }
+                        throw std::runtime_error(s);
+                    }
+
+                    // The non-permutation stuff needs only to be done once 
+                    if (isFirstRound) { 
+                        itLocus->set_polymorph(data.isPolymorph()); 
+                        itLocus->set_maf(data.maf(par_->marker_type)); 
+
+                        bool hasPassed = true;
+                        ptr_vector<Locus_filter>::iterator itFilter = 
+                            locus_filters.begin();
+                        for (; itFilter != locus_filters.end(); ++itFilter) {
+                            if (not (*itFilter)(*itLocus)) {
+                                hasPassed = false;
+                                break;
+                            }
+                        }
+
+                        if (hasPassed) {
+                            itLocus->add_test_stats(stat.test(data)); 
+                        }
+                    }
+                    if (itLocus->hasTeststat()) {
+                        stat.permutation_test(data);   
+                    }
+                    if (show_progress) {
+                        ++(*pprogress);
+                    }
+                    itLocus++;
+                }
+            }
+            perm_todo -= nperm;
+            copy(stat.tmax_begin(), stat.tmax_end(), back_inserter(tmax));
+            isFirstRound = false;
+        }
+        out_ << endl;
+        result_to_console(par_, out_, *study_);
+
+        // Get tmax of original data and use permutation tmax to derive p-values
+        out_ << normal << stdpre << "Writing results." << endl;
+        deque<double> t_orig(study_->m());
+        transform(study_->begin(), study_->end(), 
+                t_orig.begin(), mem_fun_ref(&Locus::tmax));
+        deque<size_t> counts = single_step_counts(t_orig, tmax);
+        std::string fn = par_->out_prefix;
+        fn.append(".all");
+        result_to_file(par_, *study_, counts, fn);
+
+        // The same but this time just for the top p-values
+        sort(study_->begin(), study_->end(), Locus_tmax_greater());
+        t_orig.resize(par_->ntop);
+        transform(study_->begin(), study_->begin()+par_->ntop, 
+                t_orig.begin(), mem_fun_ref(&Locus::tmax));
+        counts = single_step_counts(t_orig, tmax);
+        fn = par_->out_prefix;
+        fn.append(".top");
+        result_to_file(par_, *study_, counts, fn);
+    }
+
+    // None-Class Functions
+    // ========================================================================
     std::vector<Individual> create_study_sample(
             detail::Parameter* par, io::Myout& myout)
     {
@@ -84,140 +244,6 @@ namespace Permory { namespace gwas {
         }
     }
 
-    //
-    //  Compute test statistics and perform permutation test
-    template<uint K, uint L> void analyze_dichotom(
-            detail::Parameter* par, io::Myout& myout, 
-            const std::vector<bool>& trait, Gwas* study,
-            std::set<char> the_domain=std::set<char>())
-    {
-        using namespace std;
-        using namespace boost;
-        using namespace io;
-        using namespace statistic;
-
-        // Prepare progress bar
-        size_t m = study->m();
-        size_t perm_todo = par->nperm_total;        //remaining permutations
-        scoped_ptr<progress_display> pprogress;
-        bool show_progress = not par->quiet;
-        if (show_progress) {
-            double d = ceil(double(perm_todo)/double(par->nperm_block));
-            pprogress.reset(new progress_display(m*size_t(d), std::cout, "","",""));
-        }
-
-        // Define locus filter (e.g. maf filter)
-        ptr_vector<Locus_filter> locus_filters;
-        locus_filters.push_back(new Maf_filter(par->min_maf, par->max_maf));
-        locus_filters.push_back(new Polymorph_filter());
-
-        deque<double> tmax;     //holds the maximum test statistic per permutation
-        statistic::Dichotom<K,L> stat(*par, trait); //computes all statistic stuff
-        permutation::Permutation pp(par->seed);     //does the shuffling/permutation
-        Gwas::iterator itLocus = study->begin();    //points to current locus
-
-        bool isFirstRound = true;
-        while (perm_todo > 0) {
-            // By default analysis is done in blocks of 10000 permutations. In
-            // each block each marker is analyzed one by one
-            size_t nperm = par->nperm_block;
-            if (nperm > perm_todo) {
-                nperm = perm_todo;
-            }
-            stat.renew_permutations(&pp, nperm, par->tail_size); //fresh random numbers
-            itLocus = study->begin(); 
-
-            BOOST_FOREACH(string fn, par->fn_marker_data) { 
-                Locus_data_reader<char> loc_reader(fn, par->undef_allele_code);
-
-                while (loc_reader.hasData()) {
-                    std::vector<char> v;
-                    loc_reader.get_next(v);
-                    Locus_data<char> data(v, par->undef_allele_code);
-                    if (data.size() == 0) {
-                        continue;
-                    }
-
-                    if (par->marker_type == genotype) {
-                        if (data.size() == 2*trait.size()) {
-                            data = data.condense_alleles_to_genotypes(2);
-                        }
-                        // Ensure that all possible genotypes appear in the domain
-                        data.add_to_domain(the_domain); 
-                    }
-
-                    if (data.size() != trait.size()) {
-                        // Provide some more information in case of this error
-                        // as it may be hard to spot in large data sets
-                        std::string s = "At marker no ";
-                        s.append(boost::lexical_cast<string>(itLocus->id()));
-                        s.append(": length of phenotype data (");
-                        s.append(boost::lexical_cast<string>(trait.size()));
-                        s.append(") does not match with length of marker data (");
-                        s.append(boost::lexical_cast<string>(data.size()));
-                        s.append(").\n");
-                        if (data.size() == 2*trait.size()) {
-                            s.append("Maybe you forgot to set option '--allelic'?\n");
-                        }
-                        throw std::runtime_error(s);
-                    }
-
-                    // The non-permutation stuff needs only to be done once 
-                    if (isFirstRound) { 
-                        itLocus->set_polymorph(data.isPolymorph()); 
-                        itLocus->set_maf(data.maf(par->marker_type)); 
-
-                        bool hasPassed = true;
-                        ptr_vector<Locus_filter>::iterator itFilter = 
-                            locus_filters.begin();
-                        for (; itFilter != locus_filters.end(); ++itFilter) {
-                            if (not (*itFilter)(*itLocus)) {
-                                hasPassed = false;
-                                break;
-                            }
-                        }
-
-                        if (hasPassed) {
-                            itLocus->add_test_stats(stat.test(data)); 
-                        }
-                    }
-                    if (itLocus->hasTeststat()) {
-                        stat.permutation_test(data);   
-                    }
-                    if (show_progress) {
-                        ++(*pprogress);
-                    }
-                    itLocus++;
-                }
-            }
-            perm_todo -= nperm;
-            copy(stat.tmax_begin(), stat.tmax_end(), back_inserter(tmax));
-            isFirstRound = false;
-        }
-        myout << endl;
-        result_to_console(par, myout, *study);
-
-        // Get tmax of original data and use permutation tmax to derive p-values
-        myout << normal << stdpre << "Writing results." << endl;
-        deque<double> t_orig(study->m());
-        transform(study->begin(), study->end(), 
-                t_orig.begin(), mem_fun_ref(&Locus::tmax));
-        deque<size_t> counts = single_step_counts(t_orig, tmax);
-        std::string fn = par->out_prefix;
-        fn.append(".all");
-        result_to_file(par, *study, counts, fn);
-
-        // The same but this time just for the top p-values
-        sort(study->begin(), study->end(), Locus_tmax_greater());
-        t_orig.resize(par->ntop);
-        transform(study->begin(), study->begin()+par->ntop, 
-                t_orig.begin(), mem_fun_ref(&Locus::tmax));
-        counts = single_step_counts(t_orig, tmax);
-        fn = par->out_prefix;
-        fn.append(".top");
-        result_to_file(par, *study, counts, fn);
-    }
-
     void gwas_analysis(detail::Parameter* par, io::Myout& myout)
     {
         using namespace std;
@@ -261,13 +287,17 @@ namespace Permory { namespace gwas {
         std::set<char> data_domain;
         switch (par->marker_type) {
             case genotype:  //2x3 contingency table analysis
+            {
                 data_domain.insert('0');
                 data_domain.insert('1');
                 data_domain.insert('2');
-                analyze_dichotom<2,3>(par, myout, trait, &study, data_domain);
+                Analyzer<2,3> analyzer(par, myout, trait, &study, data_domain);
+                analyzer.analyze_dichotom();
                 break;
+            }
             case allelic: //2x2 contingency table analysis
-                analyze_dichotom<2,2>(par, myout, trait, &study);
+                Analyzer<2,2> analyzer(par, myout, trait, &study);
+                analyzer.analyze_dichotom();
                 break;
         }
     }
